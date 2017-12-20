fee91ce1884097b7ae568fd919a2b8b719665d66
Adrian L Lange <mail@p3lim.net>
2017-12-20 12:23:35 +0100

    :sparkles:

---
+++ PixelFont.lua
@@ -0,0 +1,23 @@
+-- I really wish we had relative paths for font/texture files.
+-- That way, this addon could be embedded as a library.
+
+local fontName = ...
+local colors = {
+	Title = NORMAL_FONT_COLOR,
+	Disabled = DISABLED_FONT_COLOR,
+	Yellow = NORMAL_FONT_COLOR,
+}
+
+local fontNormalName = fontName .. 'Normal'
+for colorName, color in next, colors do
+	local font = CreateFont(fontName .. colorName)
+	font:CopyFontObject(fontNormalName)
+	font:SetTextColor(color:GetRGB())
+end
+
+-- register with SharedMedia
+local LSM = LibStub('LibSharedMedia-3.0')
+if(LSM) then
+	LSM:Register(LSM.MediaType.FONT, 'Semplice', [[Interface\AddOns\PixelFont\fonts\semplice.ttf]])
+	LSM:Register(LSM.MediaType.FONT, 'Homespun', [[Interface\AddOns\PixelFont\fonts\homespun.ttf]])
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# PixelFont
+
+This is an addon for the popular MMORPG "World of Warcraft".  
+It can be downloaded at [Curse](//www.curseforge.com/wow/addons/pixelfont) or at [WoWInterface](http://wowinterface.com/downloads/info00000).
+
+## Description
+
+A simple font library that contains latin- and cyrillic-supported "pixel"-fonts.
+
+## Feedback
+
+If you have a question, please use the comments section on Curse/WoWInterface.  
+If you would like to report a bug, please follow [this link](//github.com/p3lim-wow/PixelFont/issues?q=) to get started.
+
+## Legal
+
+Please see the [LICENSE](//github.com/p3lim-wow/PixelFont/blob/master/LICENSE.txt) file.
